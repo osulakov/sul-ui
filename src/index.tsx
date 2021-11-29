a1ec36fc6cdfd57f8aee08a9ff50f3cc7cade694
Oleksandr  Sulakov <oleksandrsulakov@oleksandrs-mbp.lan>
2021-11-28 16:58:39 -0800

    multiple exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import { Button } from "./components/Button/Button";
+// import { Button } from "./components/Button/Button";
 
-const SUIButton = (): JSX.Element => {
-  return <Button />;
-};
-
-export default SUIButton;
+export { Button } from "./components/Button/Button";
